c5381007cec06a559358477c158861b5085d3dcc
parini-12 <104002931+parini-12@users.noreply.github.com>
2022-11-04 17:51:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload(){    
+}
+function setup(){
+    canvas=createCanvas(640,840);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+
+    tint_color="";
+}
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_color);
+
+  fill(255, 0, 0);
+  stroke(255, 0, 0);
+  circle(50, 50, 80);
+
+  circle(50, 430, 80);
+
+  circle(590, 50, 80);
+
+  circle(590, 430, 80);
+
+  fill(0, 128, 0);
+  stroke(0, 128, 0);
+  rect(90, 40, 460, 20);
+
+  
+  rect(90, 420, 460, 20);
+
+  rect(40, 90, 20, 300);
+
+  rect(580, 90, 20, 300);
+
+}
+function take_snapshot(){
+ save('parini_frame.png')
+
+}
+function filter_tint(){
+    tint_color=document.getElementById("color_name").value;
+}
